ada4ac100dd891c5080498904c94dcdb5275ffc0
Jackerman09 <jackerman09@gmail.com>
2018-06-20 13:18:29 -0400

    add reset to counter model

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,25 +1,31 @@
 class Counter < ApplicationRecord
 	has_many :occurrences
 
-	def current_count
+	def calculated_count
 		self.occurrences.is_increment.count - self.occurrences.is_decrement.count
 	end
 
 	def increment
 		occurrence = self.occurrences.build(is_increment: true)
-		count_field = self.count
+		count = self.count
 		if occurrence.save
-			self.count = count_field + 1
+			self.count = count + 1
 			self.save
 		end
 	end
 
 	def decrement
 		occurrence = self.occurrences.build(is_increment: false)
-		count_field = self.count
+		count = self.count
 		if occurrence.save
-			self.count = count_field - 1
+			self.count = count - 1
 			self.save
 		end
+	end
+
+	def reset
+		self.occurrences.destroy_all
+		self.count = 0
+		self.save
 	end
 end
