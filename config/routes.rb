600355cd540151ad42f6868e1f247a10d12c6caf
Jackerman09 <jackerman09@gmail.com>
2018-06-20 13:34:58 -0400

    add member routes for increment, decrement and reset to counter resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   namespace :api, defaults: { format: :json } do
-    resources :counters, only: [ :index, :show, :update ]
+    resources :counters, only: [:index, :show, :update]  do
+    	member do
+    		post 'increment'
+    		post 'decrement'
+    		post 'reset'
+    	end
+    end
   end
 end
