30ed7cc031841862ca09f11cd54f1285d4ca5b45
Jackerman09 <jackerman09@gmail.com>
2018-06-24 19:48:37 -0400

    change app to extend react.component

--- app/javascript/counters/components/App.jsx
+++ app/javascript/counters/components/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom'
 import Counter from './Counter'
 import { Counters } from './Counters'
 
-const App = (props) => (
-  <Router>
-	<div>
-	  <Route path='/' component={Counter} />
-	</div>
-  </Router>
-)
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return (
+			<Router>
+				<div>
+					<ul>
+				    <li><Link to="/">Home</Link></li>
+				    <Link to={{
+						  pathname: '/',
+						  search: '?counter=2'
+						}}>Number 2</Link>
+				    <li><Link to="/topics">Topics</Link></li>
+				  </ul>
+
+				  <hr/>
+
+				  <Route path='/' component={Counter} />
+				  <Counters />
+				</div>
+			  </Router>
+			)
+	}
+}
 
 // You will need this on the bottom of each component file
 // to make it available through ES6 'import' statements.
